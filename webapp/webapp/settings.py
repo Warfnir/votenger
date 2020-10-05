ba850c521c6d22718477bb787028a7a9d90415fb
warfnir <kamil.polokpl@gmail.com>
2020-10-05 22:42:06 +0200

    connected db, createt users

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -11,10 +11,14 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import json
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# CREDENTIALS
+with open(BASE_DIR.parent / 'credentials.json') as file:
+    CREDENTIALS = json.load(file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -37,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'voter',
 ]
 
 MIDDLEWARE = [
@@ -72,11 +78,15 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+DB = CREDENTIALS.get('DATABASE')
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': DB.get("NAME"),
+        'USER': DB.get("USER"),
+        'PASSWORD': DB.get("PASSWORD"),
+        'HOST': DB.get("HOST"),
+        'PORT': '5432',
     }
 }
 
@@ -118,3 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+AUTH_USER_MODEL = 'voter.MyUser'
